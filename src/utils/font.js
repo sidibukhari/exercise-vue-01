bf4e5053a6ffad9a5a71a9cac898cb484e68b77c
Sidi Bukhari <sidi_bukhari@yahoo.com>
2019-08-16 18:11:07 +0800

    Delete font.js

--- src/utils/font.js
+++
@@ -1,9 +0,0 @@
-import Vue from 'vue'
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { faEyeSlash, faEye } from '@fortawesome/free-solid-svg-icons'
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-
-library.add(faEye)
-library.add(faEyeSlash)
-
-Vue.component('font-awesome-icon', FontAwesomeIcon)
