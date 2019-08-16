6debeaaa9544a4810e9e87b01f3cdea9d841d404
Sidi Bukhari <sidi_bukhari@yahoo.com>
2019-08-16 18:33:46 +0800

    Modify Login view

--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import store from '@/store/'
 import Home from '@/views/Home.vue'
-import Login from '@/views/Login.vue'
+import Login from '@/views/Login'
 
 Vue.use(Router)
 
---
+++ src/views/Login/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <LoginForm></LoginForm>
+  </div>
+</template>
+
+<script>
+import LoginForm from './LoginForm'
+
+export default {
+  components: {
+    LoginForm
+  }
+}
+</script>
+
+<style>
+</style>
