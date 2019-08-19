8470809c57c3a9344cd60c0e37ea59c8ec736b9a
Sidi Bukhari <sidi_bukhari@yahoo.com>
2019-08-19 18:09:11 +0800

    initial change for today

---
+++ src/components/Modal.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <component v-if="modalToggle" v-bind:is="{modalToggle}"></component>
+    <div class="modal" tabindex="-1" role="dialog">
+      <div class="modal-dialog" role="document">
+        <div class="modal-content">
+          <component v-bind:is="modalContent"></component>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    modalToggle: {
+      type: Object
+    },
+    modalContent: {
+      type: Object
+    }
+  }
+}
+</script>
--- src/components/SideMenu.vue
+++ src/components/SideMenu.vue
@@ -22,6 +22,7 @@
 </template>
 
 <script>
+import Modal from './Modal'
 export default {
   data: function () {
     return {
